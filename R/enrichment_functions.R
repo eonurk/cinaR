bb0a2eeb1c544203af993cc89298f2601bc69e4c
Onur Karakaslar <karako@MLG-JGM123.home>
2020-11-01 22:06:37 -0500

    * Added RNA-Seq experiment * Added examples to exported functions

--- R/enrichment_functions.R
+++ R/enrichment_functions.R
@@ -7,6 +7,17 @@
 #' @param geneset Pathways to be used in enrichment analyses. If not set vp2008 (Chaussabel, 2008)
 #' immune modules will be used. This can be set to any geneset using `read.gmt` function from `qusage`
 #' package. Different modules are available: https://www.gsea-msigdb.org/gsea/downloads.jsp.
+#'
+#' @examples
+#' \donttest{
+#' library(cinaR)
+#'
+#' data("VP2008")
+#' genes.to.test <- vp2008[[1]][1:10]
+#' HPEA(genes.to.test,vp2008, background.genes.size = 20e3)
+#' }
+#'
+#' @export
 HPEA <- function(genes, geneset, background.genes.size) {
   res.hpea <- lapply(geneset, function(module) {
     # Module gene count
@@ -27,8 +38,8 @@ HPEA <- function(genes, geneset, background.genes.size) {
              k,
              background.genes.size - k,
              n,
-             lower.tail = F,
-             log.p = F)
+             lower.tail = FALSE,
+             log.p = FALSE)
 
     # take the overlapping genes for the modules
     og <- genes[genes %in% module]
@@ -61,15 +72,31 @@ HPEA <- function(genes, geneset, background.genes.size) {
 
 
 #' GSEA
+#' Having run the differential analyses this function runs geneset enrihment analyses with
+#' `fgsea` package.
 #'
 #' @param genes DA gene names to be checked if they are over-represented or not.
 #' @param geneset Pathways to be used in enrichment analyses. If not set vp2008 (Chaussabel, 2008)
 #' immune modules will be used. This can be set to any geneset using `read.gmt` function from `qusage`
 #' package. Different modules are available: https://www.gsea-msigdb.org/gsea/downloads.jsp.
-#' TODO: add ... to parameters
+#' @examples
+#' \donttest{
+#' library(cinaR)
+#' library(fgsea)
+#' data(examplePathways)
+#' data(exampleRanks)
+#' GSEA(exampleRanks, examplePathways)
+#' }
+#' @references
+#' G. Korotkevich, V. Sukhov, A. Sergushichev. Fast gene set enrichment analysis. bioRxiv (2019),
+#' doi:10.1101/060012
 #' @export
 GSEA <- function(genes, geneset) {
-  fgseaRes <- fgsea::fgsea(geneset, genes)
+  fgseaRes <- fgsea:: fgsea(pathways = geneset,
+                            stats    = genes,
+                            eps      = 0.0,
+                            minSize  = 15,
+                            maxSize  = 500)
 
   # Unlist the genes inside fgsea object to make it saveable.
   fgseaRes$leadingEdge <- vapply(fgseaRes$leadingEdge,function(x){paste(x,collapse = ",")},
@@ -83,6 +110,7 @@ GSEA <- function(genes, geneset) {
 #' @param results list, DA peaks list for different contrasts
 #' @param enrichment.method There are two methodologies for enrichment analyses,
 #' Hyper-geometric p-value (HPEA) or Geneset Enrichment Analyses (GSEA).
+#' @param experiment.type The type of experiment either set to "ATAC-Seq" or "RNA-Seq"
 #' @param enrichment.FDR.cutoff FDR cut-off for enriched terms, p-values
 #' are corrected by Benjamini-Hochberg procedure
 #' @param reference.genome genome of interested species. It should be 'hg38', 'hg19' or 'mm10'.
@@ -91,12 +119,21 @@ GSEA <- function(genes, geneset) {
 #' @param geneset Pathways to be used in enrichment analyses. If not set vp2008 (Chaussabel, 2008)
 #' immune modules will be used. This can be set to any geneset using `read.gmt` function from `qusage`
 #' package. Different modules are available: https://www.gsea-msigdb.org/gsea/downloads.jsp.
+#'
+#' @examples
+#' ## For ATAC-Seq experiments
+#' ## run_enrichment(results)
+#'
+#' ## For RNA-Seq experiments
+#' ## run_enrichment(results, experiment.type = "RNA-Seq")
+#' @export
 run_enrichment <- function (
   results,
   geneset = NULL,
+  experiment.type = "ATAC-Seq",
   reference.genome = NULL,
   enrichment.method = NULL,
-  enrichment.FDR.cutoff = 0.05,
+  enrichment.FDR.cutoff = 1,
   background.genes.size = 20e3
 ) {
 
@@ -109,91 +146,158 @@ run_enrichment <- function (
     geneset <- cinaR::vp2008
   }
 
-
-  if (is.null(reference.genome)){
-    message(">> `reference.genome` is not specified, so DA genes are assumed to be human")
-  }
   if (is.null(enrichment.method)) {
     message(">> enrichment.method` is not selected. Hyper-geometric p-value (HPEA) will be used!")
     enrichment.method <- "HPEA"
   }
-  # Convert mice gene symbols to human's
-  if (reference.genome == "mm10") {
-    # all.genes <- sapply(results, function(x) {
-    #   x["gene_name"]
-    # })
-    # all.genes <- unlist(all.genes)
-    # all.genes <- unique(all.genes)
-    #
-    # # Create mapping for all mice genes
-    # mice2humanMap <- cinaR::mouse2human(genes = all.genes)
 
+  if (reference.genome == "mm10") {
+
+    # mm10 map
     mice2humanMap <- cinaR::grcm38
 
-    # Convert to human homologs for enrichment analyses
-    results <- lapply(results, function(x) {
-      m <- match(x[, "gene_name"], mice2humanMap[, "symbol"])
-      mapped.genes <- mice2humanMap[, "HGNC.symbol"][m]
+    # If it's an RNA-seq experiment and ensembl ids are used!
+    if(experiment.type == "RNA-Seq" & grepl("ENSMUS", results[[1]][1,1], fixed = TRUE) ){
 
-      ## Careful: some genes may not be mappable!
-      ## x[,"gene_name"] <- ifelse(is.na(mapped.genes),
-      ##                          x[,"gene_name"], mapped.genes)
-      x[, "gene_name"] <- mapped.genes
-      cat(">> Human gene symbols are converted to mice!\n")
-      return(x)
-    })
+      results <- lapply(results, function(x) {
+        m <- match(x[, "gene_name"], mice2humanMap[, "ensgene"])
+        mapped.genes <- mice2humanMap[, "HGNC.symbol"][m]
+
+        x[, "gene_name"] <- mapped.genes
+        message(">> Mice ensembl ids are converted to human symbols!")
+        return(x)
+      })
+
+    } else { # If ATAC-Seq or gene-symbol are given
+
+      # Convert to human homologs for enrichment analyses
+      results <- lapply(results, function(x) {
+
+        m <- match(x[, "gene_name"], mice2humanMap[, "symbol"])
+        mapped.genes <- mice2humanMap[, "HGNC.symbol"][m]
+
+        ## Careful: some genes may not be mappable!
+        ## x[,"gene_name"] <- ifelse(is.na(mapped.genes),
+        ##                          x[,"gene_name"], mapped.genes)
+        x[, "gene_name"] <- mapped.genes
+
+        return(x)
+      })
+
+      message(">> Mice gene symbols are converted to human symbols!")
+    }
+
+
+  } else if (reference.genome == "hg38" & experiment.type == "RNA-Seq"){
+
+    ens2gene <- cinaR::grch38
+
+    if(grepl("ENSG", results[[1]][1,1], fixed = TRUE)){
+      results <- lapply(results, function(x) {
+        m <- match(x[, "gene_name"], ens2gene[, "ensgene"])
+        mapped.genes <- ens2gene[, "symbol"][m]
+
+        x[, "gene_name"] <- mapped.genes
+        if(nrow(x) == sum(is.na(x[,"gene_name"])) & nrow(x) > 5){
+          warning("Please make sure you are using the correct `reference.genome`!")
+        }
+        return(x)
+      })
+      message(">> Human ensembl ids are converted to symbols...")
+    }
+  } else if (reference.genome == "hg19" & experiment.type == "RNA-Seq"){
+    ens2gene <- cinaR::grch37
+    if(grepl("ENSG", results[[1]][1,1], fixed = TRUE)){
+      results <- lapply(results, function(x) {
+        m <- match(x[, "gene_name"], ens2gene[, "ensgene"])
+        mapped.genes <- ens2gene[, "symbol"][m]
+
+        x[, "gene_name"] <- mapped.genes
+        return(x)
+      })
+      message(">> Human ensembl ids are converted to symbols...")
+    }
   }
 
+  # remove NA rows
   results <- lapply(results, function(x){
-    # remove NA rows
-    x <- x[!is.na(x[, "gene_name"]),]
-  })
+
+      if (length(x) > 0){ # check empty DA peaks
+        x <- x[!is.na(x[, "gene_name"]),]
+
+        if (experiment.type == "ATAC-Seq"){
+          x <- x[order(abs(x[,"distanceToTSS"])),]
+          x <- x[!duplicated(x[,"gene_name"]),]
+        }
+      }
+    }
+  )
 
   if (enrichment.method == "HPEA") {
     enrichment.results <-
     lapply(results, function(x) {
-      opening.locs <- x[, "logFC"] > 0
-      genes.opening <- x[ opening.locs, ]
-      genes.closing <- x[!opening.locs, ]
 
-      enrichment.opening <-
-        HPEA(
-          genes = genes.opening[, "gene_name"],
-          geneset = geneset,
-          background.genes.size = background.genes.size
-        )
+      if(is.null(x)){
+        enrichment.results <- list()
+      } else{
+        opening.locs <- x[, "logFC"] > 0
+        genes.opening <- x[ opening.locs, ]
+        genes.closing <- x[!opening.locs, ]
 
-      enrichment.closing <-
-        HPEA(
-          genes = genes.closing[, "gene_name"],
-          geneset = geneset,
-          background.genes.size = background.genes.size
-        )
+        enrichment.opening <-
+          HPEA(
+            genes = genes.opening[, "gene_name"],
+            geneset = geneset,
+            background.genes.size = background.genes.size
+          )
 
-      enrichment.results <-
-        rbind(
-          cbind(enrichment.opening, status = "Opening"),
-          cbind(enrichment.closing, status = "Closing")
-        )
+        enrichment.closing <-
+          HPEA(
+            genes = genes.closing[, "gene_name"],
+            geneset = geneset,
+            background.genes.size = background.genes.size
+          )
 
-      enrichment.results <-
-        enrichment.results[enrichment.results[,"adj.p"] <= enrichment.FDR.cutoff,]
+        enrichment.results <-
+          rbind(
+            cbind(enrichment.opening, status = "Opening"),
+            cbind(enrichment.closing, status = "Closing")
+          )
 
-      rownames(enrichment.results) <- NULL
+        enrichment.results <-
+          enrichment.results[enrichment.results[,"adj.p"] <= enrichment.FDR.cutoff,]
 
+        rownames(enrichment.results) <- NULL
+      }
       return(enrichment.results)
     })
+
+
   } else if (enrichment.method == "GSEA") {
     enrichment.results <- lapply(results, function(x){
-      rank <- x[,"logFC"]
-      names(rank) <- x[,"gene_name"]
-      res <- as.data.frame(GSEA(genes = rank, geneset = geneset))
-      res <- res[res[,"padj"] <= enrichment.FDR.cutoff,]
+
+      if(length(x) > 0) { # check non DA peaks
+        rank <- x[,"logFC"]
+        names(rank) <- x[,"gene_name"]
+        res <- as.data.frame(GSEA(genes = rank, geneset = geneset))
+        res <- res[res[,"padj"] <= enrichment.FDR.cutoff,]
+
+      } else{
+        res <- data.frame()
+      }
+
       return(res)
     })
   } else {
     stop("`enrichment.method` should be either `HPEA` or `GSEA`")
   } # end-if (enrichment.method)
 
+  # remove empty enrichment results
+  enrichment.results <- Filter(function(x){length(x) >0}, enrichment.results)
+
+  if(length(enrichment.results) == 0){
+    warning(">> There are no enriched pathways for any of the comparisons!
+            Maybe less stringent `DA.fdr.threshold` or `DA.lfc.threshold` values?")
+  }
   return(enrichment.results)
 }
